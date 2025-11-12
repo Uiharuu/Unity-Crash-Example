8145afdd4b015939b26f628fb5d1005791df7f44
Uiharu <867498211@qq.com>
2025-11-12 15:25:42 +0800

    leeson 17

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    [SerializeField] float redDuration = 1;
+    [SerializeField] float timer;
+    [SerializeField] float currentTime;
+    [SerializeField] float lastDamagedTime;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        currentTime = Time.time;
+        
+        if (currentTime > lastDamagedTime + redDuration)
+        {
+            sr.color = Color.white;
+        }
+    }
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name + "get attacked!");
+        if (sr.color == Color.white)
+        {
+            Debug.Log(gameObject.name + "get attacked!");
+            sr.color = Color.red;
+
+            lastDamagedTime = Time.time;
+        }
     }
+
 }
